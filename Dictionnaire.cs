7262f16c9abba0124033da5db2f972c3632236dd
naim90 <naim.elmoudni@gmail.com>
2021-10-05 11:59:09 +0200

    dictio

---
+++ Dictionnaire.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionnaire
+{
+    class Dictionnaire
+    {
+        public static void Dictio()
+        {
+            Dictionary<char,List<string>> dictionnaire = new Dictionary<char, List<string>>();
+            dictionnaire['b'] = new List<string> { "battre","boire","barboter" };
+            dictionnaire['a'] = new List<string> {"adorer","apporter","améliorer"};
+            dictionnaire['c'] = new List<string> { "croire", "crier", "cracher" };
+
+            dictionnaire.Remove('c');
+
+            Console.WriteLine(dictionnaire['a']);
+            Console.WriteLine(dictionnaire['b']);
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dictionnaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionnaire.Dictio();
+        }
+    }
+}
